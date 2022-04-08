181c8c66651545cf9679aa913b3d9163fd7f3786
gholloway32 <97569235+gholloway32@users.noreply.github.com>
2022-04-08 18:28:58 -0400

    project 5

--- 05-FPS-Maze/Enemys/Enemy.gd
+++ 05-FPS-Maze/Enemys/Enemy.gd
@@ -1,8 +1,16 @@
 extends Area
 
+var Player = null
+
 
 func _ready():
 	pass
+
+func _physics_process(_delta):
+	if Player == null:
+		Player = get_node_or_null("/root/Game/Player")
+		if Player != null:
+			look_at(Player.global_transform.origin, Vector3.UP)
 
 
 func _on_Enemy_body_entered(body):
@@ -10,4 +18,10 @@ func _on_Enemy_body_entered(body):
 		var sound = get_node_or_null("/root/Game/Zombie")
 		if sound != null:
 			sound.playing = true
-		queue_free()
+		var _scene = get_tree().change_scene("res://UI/Lose.tscn")
+
+
+func _on_Area_body_entered(body):
+	if body.name == "Player":
+		var sound = get_node_or_null("/root/Game/Zombie")
+		sound.playing = true
